3628a2607dbc54fb179539effb8923fc4ef7f01a
yuhengo <yuhengo@students.cs.ubc.ca>
2021-07-24 01:27:12 +0800

    Implemented methods for Calendar

--- src/model/Calendar.java
+++ src/model/Calendar.java
@@ -35,39 +35,71 @@ public class Calendar {
     // MODIFIES: this
     // EFFECTS: adds entry to this.entries
     public void addEntry(Entry entry) {
-        // stub
+        entries.add(entry);
     }
 
     // REQUIRES: entry is already in this.entries
     // MODIFIES: this
     // EFFECTS: removes entry from this.entries
     public void removeEntry(Entry entry) {
-        // stub
+        entries.remove(entry);
     }
 
     // EFFECTS: returns list of all meetings
     public List<Meeting> getMeetings() {
-        return null; // stub
+        List<Meeting> meetings = new ArrayList<>();
+        for (Entry e : entries) {
+            if (e instanceof Meeting) {
+                meetings.add((Meeting) e);
+            }
+        }
+        return meetings;
     }
 
     // REQUIRES: an entry with label is already in this.entries
     // EFFECTS: returns entry with given label
     public Entry getEntry(String label) {
-        return null; // stub
+        for (Entry e : entries) {
+            if (e.getLabel().equals(label)) {
+                return e;
+            }
+        }
+        return null;
     }
 
     // EFFECTS: prints out dates, times, labels, whether repeating of all entries
     public void printEntries() {
-        // stub
+        for (Entry e : entries) {
+            String repLabel;
+            if (e.isRepeating()) {
+                repLabel = "Yes";
+            } else {
+                repLabel = "No";
+            }
+            System.out.println(e.getLabel() + "\t" + e.getDate().getShortFormat() + " " +
+                    e.getTime().getTime() + "\tRepeating: " + repLabel);
+        }
     }
 
     // EFFECTS: prints out dates, time, labels, and special notes of all reminders for events
     public void printEventReminders() {
-        // stub
+        for (Entry e : entries) {
+            if (e instanceof Reminder) {
+                System.out.println("!!!Reminder: " + e.getDate().getShortFormat() + " " +
+                        e.getTime().getTime() + " " + ((Reminder) e).getNote());
+            } else if (e instanceof Event) {
+                if (((Event) e).getReminder() != null) {
+                    System.out.println("!!!Reminder: " + e.getDate().getShortFormat() + " " +
+                            e.getTime().getTime() + " " + ((Event) e).getReminder().getNote());
+                }
+            }
+        }
     }
 
     // EFFECTS: sends invitations for all meetings
     public void inviteMeetings() {
-        // stub
+        for (Meeting m : getMeetings()) {
+            m.sendInvites();
+        }
     }
 }
